bcc263e3a0214c7b68f54a49a515d761fff66493
Lihogub Oleg <oligli@yandex.ru>
2021-09-06 19:56:08 +0300

    [lesson 1][homework] third task

--- lesson01/homework.py
+++ lesson01/homework.py
@@ -23,3 +23,21 @@ def two(_list):
 
     print(c)
 
+
+# print minimal distance between local maximums of list
+def three(_list):
+    l_max_list = []
+
+    for i in range(1, len(_list) - 1):
+        if (_list[i-1] < _list[i]) and (_list[i] > _list[i+1]):
+            l_max_list.append(i)
+
+    l_dist = len(_list)
+
+    for i in range(1, len(l_max_list)):
+        l_dist = min(l_dist, l_max_list[i] - l_max_list[i-1])
+
+    if l_dist == len(_list):
+        print("Not enough maximums")
+    else:
+        print(l_dist)
