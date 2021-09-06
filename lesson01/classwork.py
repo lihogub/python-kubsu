cd1f0f565698ece322db7745656d1bc80a3f0a14
Lihogub Oleg <oligli@yandex.ru>
2021-09-06 19:01:06 +0300

    [classwork] first task

---
+++ lesson01/classwork.py
@@ -0,0 +1,11 @@
+# print each number from A to B (including)
+def one():
+    a = int(input("a: "))
+    b = int(input("b: "))
+    for i in range(a, b + 1):
+        print(i)
+
+
+#   [print(i) for i in range(int(input("a: ")), int(input("b: ")) + 1)]
+
+
