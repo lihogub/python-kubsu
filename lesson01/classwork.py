17b1482dfa70450b944f6d1a2a9c0f2ddf7e1ced
Lihogub Oleg <oligli@yandex.ru>
2021-09-06 19:01:34 +0300

    [lesson 1][classwork] third task

--- lesson01/classwork.py
+++ lesson01/classwork.py
@@ -26,3 +26,17 @@ def two():
 
 #   (lambda r, a, b: [print(i) for i in range(a, b + 1) if (r == sum(map(int, str(i))))])(int(input("r: ")), int(input("a: ")), int(input("b: ")))
 
+
+# print each number from A to B (including) if it is palindrome and it contains three equal digits
+def three():
+    def check_digit_count(num, count):
+        return len([i for i in range(10) if (str(num).count(str(i)) == count)]) > 0
+
+    a = int(input("a: "))
+    b = int(input("b: "))
+
+    for i in range(a, b + 1):
+        if check_digit_count(i, 3) and str(i) == str(i)[::-1]:
+            print(i)
+
+#   [print(i) for i in range(int(input("a: ")), int(input("b: ")) + 1) if (lambda num, count: len([i for i in range(10) if (str(num).count(str(i)) == count)]) > 0)(i, 3) and str(i) == str(i)[::-1]]
