ebd1ba04576f96332c80ccd7a544363c00a002ec
Lihogub Oleg <oligli@yandex.ru>
2021-09-28 15:26:56 +0300

    [individual] task 1

---
+++ individual/individual_task_01.py
@@ -0,0 +1,24 @@
+import random
+
+
+def task01():
+    n = int(input("N:"))
+    number_list = [int(input()) for i in range(n)]
+    digit_map = {}
+
+    for number in number_list:
+        for digit in hex(number)[2:]:
+            if digit in digit_map:
+                digit_map[digit] += 1
+            else:
+                digit_map[digit] = 1
+
+    even_odd_list = list(map(lambda x: x % 2, digit_map.values()))
+    odd = even_odd_list.count(1)
+    if odd <= 1:
+        print(1)
+    else:
+        print(0)
+
+
+task01()
